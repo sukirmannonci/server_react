8e87b6b8b41eb7e5b958fd7c06f2360182f56482
sukirmannonci <sukirman.nonci@gmail.com>
2021-08-22 19:28:55 +0800

    berhasil menambahkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./rest');
 var connection = require('./connect');
 
 exports.index = function(req, result){
-    response.ok('Aplikasi rest api sedang berjalan')
+    response.ok('Aplikasi rest api sedang berjalan', result)
+};
+
+// Menampilkan semua data mahasiswa
+exports.tampilSemuaDataMahasiswa = function(req, result){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error)
+        }else{
+            response.ok(rows, result)
+        }
+    });
 };
