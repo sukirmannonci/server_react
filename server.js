08e202a7150c8bec4f61d19af59265b2f081ed07
sukirmannonci <sukirman.nonci@gmail.com>
2021-08-22 19:15:18 +0800

    setting route di server js

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
-// parser application json
+// Parser application json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Panggil route
+var route = require('./route');
+route(app);
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
